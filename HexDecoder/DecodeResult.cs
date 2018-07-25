2aa2e8fe36fdef005f286c2ae55c0a57e0e89216
Alexey Astakhov <aastv@yandex.ru>
2018-07-26 00:50:36 +0300

    Begin decoder

--- HexDecoder/DecodeResult.cs
+++ HexDecoder/DecodeResult.cs
@@ -12,29 +12,82 @@ namespace HexDecoder
     public class DecodeResult
     {
         /// <summary>
-        /// Представление в виде последовательности байт.
+        /// Хранит значение <see cref="Bytes"/>
         /// </summary>
-        private byte[] bytes;
+        private List<byte> bytes;
 
         /// <summary>
-        /// Представление в виде строки.
+        /// Хранит значение <see cref="OutString"/>
         /// </summary>
         private string outString;
 
         /// <summary>
-        /// Последовательность байт в виде текста.
+        /// Хранит значение <see cref="BytesAsText"/>
         /// </summary>
         private List<string> bytesAsText;
 
         /// <summary>
+        /// Хранит значение <see cref="Errors"/>
+        /// </summary>
+        private List<string> errors;
+
+        /// <summary>
         /// Конструктор с параметрами.
         /// </summary>
-        /// <param name="bytes"></param>
-        /// <param name="outString"></param>
-        /// <param name="bytesAsText"></param>
-        public DecodeResult(byte[] bytes, string outString, List<string> bytesAsText)
+        /// <param name="bytes">Представление в виде последовательности байт.</param>
+        /// <param name="outString">Представление в виде строки.</param>
+        /// <param name="bytesAsText">Последовательность байт в виде текста.</param>
+        /// <param name="errors">Ошибки декодирования.</param>
+        public DecodeResult(List<byte> bytes, string outString, List<string> bytesAsText, List<string> errors)
         {
+            this.bytes = bytes;
+            this.outString = outString;
+            this.bytesAsText = bytesAsText;
+            this.errors = errors;
+        }
 
+        /// <summary>
+        /// Представление в виде последовательности байт.
+        /// </summary>
+        public List<byte> Bytes
+        {
+            get
+            {
+                return this.bytes;
+            }
+        }
+
+        /// <summary>
+        /// Представление в виде строки.
+        /// </summary>
+        public string OutString
+        {
+            get
+            {
+                return this.outString;
+            }
+        }
+
+        /// <summary>
+        /// Последовательность байт в виде текста.
+        /// </summary>
+        public List<string> BytesAsText
+        {
+            get
+            {
+                return this.bytesAsText;
+            }
+        }
+
+        /// <summary>
+        /// Ошибки декодирования.
+        /// </summary>
+        public List<string> Errors
+        {
+            get
+            {
+                return this.errors;
+            }
         }
     }
 }
---
+++ HexDecoder/Parser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexDecoder
+{
+    /// <summary>
+    /// Класс статического парсера.
+    /// </summary>
+    public static class Parser
+    {
+        private static List<byte> bytes = new List<byte>();
+        private static List<string> bytesAsText = new List<string>();
+        private static string outString = string.Empty;
+
+        /// <summary>
+        /// Метод декодирования данных в виде строки, 
+        /// представляющей последовательность HEX-кодов.
+        /// </summary>
+        /// <param name="inputText">Декодируемый текст.</param>
+        /// <returns>Результат декодирования.</returns>
+        public static DecodeResult Decode(string inputText)
+        {
+            var separator = string.Empty;
+            var clearedText = inputText.Replace("\n", "").Replace("\t", "").Replace(" ","");
+            var tempByteAsString = string.Empty;
+
+            for (int i = 0; i < clearedText.Length; i++)
+            {
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Метод декодирования данных в виде последовательности байт.
+        /// </summary>
+        /// <param name="inputBytes">Декодируемые данные.</param>
+        /// <returns>Результат декодирования.</returns>
+        public static DecodeResult Decode(byte[] inputBytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
