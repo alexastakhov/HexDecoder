de4543112e8c588be535a2af4f8786c0668a9359
Alexey Astakhov <aastv@yandex.ru>
2018-07-25 23:33:24 +0300

    Initial commit

---
+++ HexDecoder/DecodeResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexDecoder
+{
+    /// <summary>
+    /// Класс результата декодирования.
+    /// </summary>
+    public class DecodeResult
+    {
+        /// <summary>
+        /// Представление в виде последовательности байт.
+        /// </summary>
+        private byte[] bytes;
+
+        /// <summary>
+        /// Представление в виде строки.
+        /// </summary>
+        private string outString;
+
+        /// <summary>
+        /// Последовательность байт в виде текста.
+        /// </summary>
+        private List<string> bytesAsText;
+
+        /// <summary>
+        /// Конструктор с параметрами.
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="outString"></param>
+        /// <param name="bytesAsText"></param>
+        public DecodeResult(byte[] bytes, string outString, List<string> bytesAsText)
+        {
+
+        }
+    }
+}
